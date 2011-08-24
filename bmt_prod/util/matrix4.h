9698c04d55073f627b0a2aa8e235847b4c255d2c
np3 <na5672@gmail.com>
2011-08-24 21:46:07 +0400

    add to Matrix4 class invertion support, also add ptr() functionality

--- bmt_prod/util/matrix4.h
+++ bmt_prod/util/matrix4.h
@@ -90,7 +90,7 @@ public:
 	{
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		M.m[3][0] = V.x;
 		M.m[3][1] = V.y;
 		M.m[3][2] = V.z;
@@ -100,7 +100,7 @@ public:
 	{
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		M.m[3][0] = tx;
 		M.m[3][1] = ty;
 		M.m[3][2] = tz;
@@ -109,7 +109,7 @@ public:
 	static Matrix4 RotationX(float ang) {
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		float c = cosf(ang), s = sinf(ang);
 		M.m[1][1] = c; M.m[1][2] = -s;
 		M.m[2][1] = s; M.m[2][2] = c;
@@ -118,7 +118,7 @@ public:
 	static Matrix4 RotationY(float ang) {
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		float c = cosf(ang), s = sinf(ang);
 		M.m[0][0] = c; M.m[0][2] = s;
 		M.m[2][0] = -s; M.m[2][2] = c;
@@ -127,7 +127,7 @@ public:
 	static Matrix4 RotationZ(float ang) {
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		float c = cosf(ang), s = sinf(ang);
 		M.m[0][0] = c; M.m[0][1] = -s;
 		M.m[1][0] = s; M.m[1][1] = c;
@@ -136,7 +136,7 @@ public:
 	static Matrix4 RotationWithAxis(const Vector3 &axis, float ang) {
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		float c = cosf(ang), s = sinf(ang);
 		float ax = axis.x, ay = axis.y, az = axis.z;
 		M.m[0][0] = c + ax * ax * (1 - c);
@@ -153,26 +153,24 @@ public:
 	static Matrix4 Scaling(const Vector3 &S) {
 		Matrix4 M;
 		
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		M.m[0][0] = S.x; M.m[1][1] = S.y; M.m[2][2] = S.z;
 		return M;
 	}
 	static Matrix4 Scaling(float sx, float sy, float sz) {
 		Matrix4 M;
 
-		M.IdentityMatrix();
+		M.MakeIdentity();
 		M.m[0][0] = sx; M.m[1][1] = sy; M.m[2][2] = sz;
 		return M;
 	}
-	static Matrix4 Inverse(const Matrix4 &rhs) {
-		Matrix4 M;
-		/*T l[4][4] = {0}, u[4][4] = {0};
+	void Invert() {
+		T l[4][4] = {{0}}, u[4][4] = {{0}}, inv_m[4][4] = {{0}};
 		
-
 		for(int j = 0; j < 4; j++)
-			u[0][j] = rhs.m[0][j];
+			u[0][j] = m[0][j];
 		for(int i = 1; i < 4; i++)
-			l[i][0] = rhs.m[i][0] / u[0][0];
+			l[i][0] = m[i][0] / u[0][0];
 		for(int i = 0; i < 4; i++)
 			l[i][i] = 1;
 		for(int i = 1; i < 4; i++)
@@ -180,7 +178,7 @@ public:
 			{
 				if(i <= j)
 				{
-					u[i][j] = rhs.m[i][j];
+					u[i][j] = m[i][j];
 					T sum = 0;
 					for(int k = 0; k < i; k++)
 						sum += l[i][k] * u[k][j];
@@ -188,43 +186,51 @@ public:
 				}
 				else
 				{
-					l[i][j] = rhs.m[i][j];
+					l[i][j] = m[i][j];
 					T sum = 0;
 					for(int k = 0; k < j; k++)
 						sum += l[i][k] * u[k][j];
-					l[j][i] -= sum;
-					l[j][i] /= u[j][j];
+					l[i][j] -= sum;
+					l[i][j] /= u[j][j];
 				}
 			}
-		for(int i = 0; i < 4; i++)
-			for(int j = 0; j < 4; j++)
+		for(int i = 3; i >= 0; i--)
+			for(int j = 3; j >= 0; j--)
 			{
 				if(i == j)
 				{
 					T sum = 0;
 					for(int k = j + 1; k < 4; k++)
-						sum += u[j][k] * M.m[k][j];
-					M.m[j][j] = (1 - sum) / u[j][j];
+						sum += u[j][k] * inv_m[k][j];
+					inv_m[j][j] = (1 - sum) / u[j][j];
 				}
 				else if(i < j) {
 					T sum = 0;
 					for(int k = i + 1; k < 4; k++)
-						sum += u[i][k] * M.m[k][j];
-					M.m[i][j] = -sum / u[i][i];
+						sum += u[i][k] * inv_m[k][j];
+					inv_m[i][j] = -sum / u[i][i];
 				}
 				else
 				{
 					T sum = 0;
 					for(int k = j + 1; k < 4; k++)
-						sum += M.m[i][k] * l[k][j];
-					M.m[i][j] = -sum;
+						sum += inv_m[i][k] * l[k][j];
+					inv_m[i][j] = -sum;
 				}
-			}*/
-			return M;
+			}
+			memcpy(m, inv_m, 16 * sizeof(T));
+	}
+	static Matrix4 Inverse(const Matrix4 &rhs) {
+		Matrix4 M;
+		
+		M = rhs;
+		M.Invert();
+		return M;
 	}
 	T& operator()(int i, int j) {
 		return m[i][j];
 	}
+	T* ptr() { return (T *)m[0]; }
 };
 
-typedef Matrix4<float> Matrix4f;
+typedef Matrix4<GLfloat> Matrix4f;
