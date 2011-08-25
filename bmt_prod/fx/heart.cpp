49b550f65937848be2835a3b8e67ad4a711d9041
Lev <lev.panov@gmail.com>
2011-08-25 21:03:14 +0400

    Heart scene shows other shader and music syncing now

--- bmt_prod/fx/heart.cpp
+++ bmt_prod/fx/heart.cpp
@@ -24,10 +24,42 @@ void Heart::draw()
 	g_params.useNamespace("heart");
 	Color4 col(1, 1, 1, 1);
 
-	Shader &heart = g_shaders.getShader("heart");
-	heart.bind();
-	heart.setUniform1f("time", float(g_system.getTime()));
-	heart.setUniform2fv("resolution", 1, res);
+//	Shader &heart = g_shaders.getShader("heart");
+//	heart.bind();
+//	heart.setUniform1f("time", float(g_system.getTime()));
+//	heart.setUniform2fv("resolution", 1, res);
+
+	float wave[3];
+	wave[0] = g_system.getSpectrumSum();
+	wave[1] = g_system.getSpectrumSum();
+	wave[2] = g_system.getSpectrumSum();
+
+//	if (fabs(oldwave - wave) > 0.1)
+//	{
+//		g_debug << "wave = " << wave << endl;
+//	}
+//	oldwave = wave;
+
+	if (!g_pitch.matchLowBassBeat())
+	{
+		wave[0] = PIf / 2;
+	}
+	if (!g_pitch.matchBassBeat())
+	{
+		wave[1] = PIf / 2;
+	}
+	if (!g_pitch.matchHiBassBeat())
+	{
+		wave[2] = PIf / 2;
+	}
+
+
+	Shader &fx_monjori = g_shaders.getShader("fx_monjori");
+	fx_monjori.bind();
+	fx_monjori.setUniform1f("time", float(g_system.getTime()));
+	fx_monjori.setUniform2fv("resolution", 1, res);
+	fx_monjori.setUniform3fv("wave", 1, wave);
+
 	glUtil::fullscreenQuad(col);
 	//uniform float time;
 	//uniform vec2 resolution;
